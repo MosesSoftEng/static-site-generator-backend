9ad6866abb682d7d956f858590a290cce7b43d79
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2023-02-11 09:39:51 +0300

    Add docs

--- src/index.ts
+++ src/index.ts
@@ -28,10 +28,24 @@ app.listen(config.port, (): void => {
 });
 
 //* Endpoints.
+/**
+ * Main endpoint for the root path of the application.
+ * @function
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {void}
+ */
 app.get('/', (req: Request, res: Response): void => {
 	res.send('Hello Typescript with Node.js!');
 });
 
+/**
+ * Endpoint for handling file uploads.
+ * @function
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {void}
+ */
 app.options('/sites/upload', cors()); // Enable pre-flight request for this endpoint.
 app.post('/sites/upload', [cors(), upload.single('file')], async (req: Request, res: Response) => {
 	const {path: uploadFilePath} = req.file!;
@@ -55,10 +69,22 @@ app.post('/sites/upload', [cors(), upload.single('file')], async (req: Request, 
 	}
 });
 
+/**
+ * Endpoint for listing all the directories in the sites directory.
+ * @function
+ * @param {Object} req - Express request object.
+ * @returns {Response} - Returns a JSON response with a message and data of the directories in the sites directory.
+ */
 app.options('/sites/', cors()); // Enable pre-flight request for this endpoint.
 app.get('/sites/', cors(), (req: Request, res: Response): Response => res.status(200)
 	.json({message: '', data: getDirectories(config.sitesDir)}));
 
+/**
+ * ConvertMarkDownToHtml - Asynchronously reads the given file, converts the markdown content
+ *  to HTML using Showdown, and saves the HTML content back to the file.
+ * @param {string} filePath The path to the file to be converted
+ * @returns {Promise} Resolves with a message indicating the file was successfully converted
+ */
 const convertMarkDownToHtml = async (filePath: string): Promise<string> => new Promise((resolve, reject) => {
 	fs.readFile(filePath, 'utf8', (err, data) => {
 		if (err) {
@@ -81,6 +107,12 @@ const convertMarkDownToHtml = async (filePath: string): Promise<string> => new P
 	});
 });
 
+/**
+ * MoveFile - Asynchronously moves the file from the originPath to the targetPath.
+ * @param {string} originPath The path to the original file
+ * @param {string} targetPath The target path for the file
+ * @returns {Promise} Resolves with a message indicating the file was successfully moved
+ */
 const moveFile = async (originPath: string, targetPath: string): Promise<string> => new Promise((resolve, reject) => {
 	const folder = targetPath.substring(0, targetPath.lastIndexOf('/'));
 
@@ -101,6 +133,11 @@ const moveFile = async (originPath: string, targetPath: string): Promise<string>
 	});
 });
 
+/**
+ * GetDirectories - Returns an array of directory names located in the given path
+ * @param {string} path The path to retrieve the directories from
+ * @returns {string[]} An array of directory names
+ */
 const getDirectories = (path: string): string[] => fs.readdirSync(path, {withFileTypes: true})
 	.filter(dirent => dirent.isDirectory())
 	.map(dirent => dirent.name);
